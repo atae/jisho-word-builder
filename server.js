3eac6a14826c924bfe481dd0cfe7c177cb89a79f
Andrew Tae <andrewtae368@gmail.com>
2018-04-30 10:25:27 -0700

    api pull working

---
+++ server.js
@@ -0,0 +1,16 @@
+var express = require('express');
+var request = require('request');
+var cors = require('cors')
+
+var app = express();
+
+app.use(cors());
+
+app.use('/', function (req, res) {
+    var url = 'http://jisho.org/api/v1/search/words' + req.url;
+    console.log(url); 
+    req.pipe(request(url)).pipe(res);
+});
+
+
+app.listen(process.env.PORT || 8080);  
--- src/App.js
+++ src/App.js
@@ -3,7 +3,25 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super()
+    this.state = {dictionary_results: {}};
+  }
+
+  componentDidMount(){
+    fetch('http://localhost:8080/?keyword=mending', { mode: 'cors' }).then((res) => {
+      let that = this;
+      res.json().then(function(resJson) {
+        let jsonResponse = {};
+        jsonResponse = resJson.data;
+        that.setState({dictionary_results: jsonResponse});
+      });
+
+    });
+  }
+
   render() {
+    console.log(this.state.dictionary_results);
     return (
       <div className="App">
         <header className="App-header">
