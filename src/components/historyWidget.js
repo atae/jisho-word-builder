7189fd6346d418f66cced364bb65efda5ff649bd
Andrew Tae <andrewtae368@gmail.com>
2018-09-04 13:44:33 -0700

    glowing logo

--- src/components/historyWidget.js
+++ src/components/historyWidget.js
@@ -18,15 +18,32 @@ class HistoryWidget extends React.Component {
         })
     }
 
+    componentDidMount = () => {
+        this.onAnimationEnd();
+    }
+
+    onAnimationEnd = () => {
+        let el = document.getElementsByClassName('history-widget')[0]
+        el.addEventListener('animationend', () => {
+            if (el.className == 'history-widget fadeOutRight') {
+                el.style.height = '0';
+            } else {
+                el.style.height = 'auto';
+            }
+        })  
+    }
+
     render() {
         console.log(this.props.searchForWords)
         return(
-            <div className={"history-widget " + this.state.listClass}>
-                <HistoryList 
-                clearHistory={this.props.clearHistory}
-                history={this.props.history}
-                 searchForWords={this.props.searchForWords}
-                listClass={this.state.listClass} />
+            <div className="history-widget-container">
+                <div className={"history-widget " + this.state.listClass}>
+                    <HistoryList 
+                    clearHistory={this.props.clearHistory}
+                    history={this.props.history}
+                    searchForWords={this.props.searchForWords}
+                    listClass={this.state.listClass} />
+                    </div>
                 <button onClick={this.onClickHandler} className={'animated fadeIn ' + this.state.buttonClass}  > History </button>
             </div>
         )   
